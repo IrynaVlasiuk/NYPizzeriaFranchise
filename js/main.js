b6ec6098a1a244c6b3bf1bb04ba8ef401477236f
IrynaVlasiuk <irynka.vlasova@gmail.com>
2022-11-11 23:02:26 +0200

    Update 2022-11-11T21:02:22.392Z

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+function changeOpacitySlider(images,indexImageDisplayed){for(let i=0;i<images.length;i++){images[i].style.opacity=0;}
+images[indexImageDisplayed].style.opacity=1;}
+function startSlider(element){let indexImageDisplayed=0;setInterval(()=>{let images=document.querySelectorAll(element);indexImageDisplayed++;if(indexImageDisplayed==(images.length-1)){indexImageDisplayed=0;}
+changeOpacitySlider(images,indexImageDisplayed);},3000);}
+startSlider("#sliderBlockFranchiseDelivery .slider-item");startSlider("#sliderBlockDishes .slider-item");function observerAnimation(toggleClass,elementsClass){const startAnimation=(entries,observer)=>{entries.forEach(entry=>{entry.target.classList.toggle(toggleClass,entry.isIntersecting);});};const observer=new IntersectionObserver(startAnimation);const options={root:null,rootMargin:'0px',threshold:1};const elements=document.querySelectorAll(elementsClass);elements.forEach(el=>{observer.observe(el,options);});}
+observerAnimation("move-from-left",".animation-move-from-left-1");observerAnimation("move-from-left",".animation-move-from-left-2");observerAnimation("move-from-right",".animation-move-from-right-1");observerAnimation("move-from-right",".animation-move-from-right-2");observerAnimation("move-from-right",".animation-move-from-right-3");observerAnimation("transition-visibility",".element-transition-visibility");observerAnimation("scale-up-to",".animation-scale-up-to");
